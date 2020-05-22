b216d68631ac10e2a9bbff8c073f0871a9039525
Rohit Kumar <kumar111222rohit@gmail.com>
2020-05-22 13:27:21 +0530

    Updates

---
+++ precache-manifest.fa1acfb29cd4dfda388bc5095a36141d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e421cdb7d08c9325d3468c3e495b70c1",
+    "url": "/Weather-Forecast/index.html"
+  },
+  {
+    "revision": "1bc341d4011baca02dc7",
+    "url": "/Weather-Forecast/static/css/main.0ba0831e.chunk.css"
+  },
+  {
+    "revision": "47e9095007f68fc1040a",
+    "url": "/Weather-Forecast/static/js/2.f8b8e8dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather-Forecast/static/js/2.f8b8e8dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc341d4011baca02dc7",
+    "url": "/Weather-Forecast/static/js/main.8fec9583.chunk.js"
+  },
+  {
+    "revision": "d55ac2e7951a5d974ede",
+    "url": "/Weather-Forecast/static/js/runtime-main.5cc0c8e3.js"
+  },
+  {
+    "revision": "f57fcd360a9cd78a193283f4c2cbb824",
+    "url": "/Weather-Forecast/static/media/weather-background.f57fcd36.jpg"
+  }
+]);
